fbc48ccc30a85a2c8dacf2bb92ca9d513a67b2a2
futao <1185172056@qq.com>
2020-05-06 18:14:45 +0800

    使用ServerSockt和Socket实现消息的发送与接收

---
+++ src/main/java/com/futao/learn/imooc/netty/MyClientSocket.java
@@ -0,0 +1,48 @@
+package com.futao.learn.imooc.netty;
+
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ * 客户端 Socket
+ *
+ * @author futao
+ * @date 2020/5/6
+ */
+public class MyClientSocket {
+
+    public static final String QUIT_KEY_WORD = "quit";
+
+    public static void main(String[] args) {
+        try (Socket socket = new Socket("127.0.0.1", MyServerSocket.SERVER_PORT)) {
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            while (true) {
+                BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+                String input = consoleReader.readLine();
+
+
+                writer.write(input + "\n");
+                writer.flush();
+
+
+                String msg = reader.readLine();
+                System.out.println("接收到服务端的消息；" + msg);
+
+                if (QUIT_KEY_WORD.equals(input)) {
+                    writer.close();
+                    reader.close();
+                    socket.close();
+                    break;
+                }
+            }
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/futao/learn/imooc/netty/MyServerSocket.java
@@ -0,0 +1,53 @@
+package com.futao.learn.imooc.netty;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * 服务端ServerSocket
+ *
+ * @author futao
+ * @date 2020/5/6
+ */
+public class MyServerSocket {
+
+    public static final int SERVER_PORT = 8888;
+
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(SERVER_PORT)) {
+            System.out.println("服务端启动，端口:" + serverSocket.getLocalPort());
+            while (true) {
+                //线程将阻塞，知道有客户端进行连接
+                Socket socket = serverSocket.accept();
+                System.out.println("接收到客户端请求." + socket.getPort());
+                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8));
+
+                String msg = null;
+                while ((msg = reader.readLine()) != null) {
+                    //读取客户端发送的消息
+                    System.out.println("[客户端" + socket.getPort() + "]：" + msg);
+                    //发送响应
+                    writer.write("来自服务端的响应:" + msg + "\n");
+                    //刷新缓冲区，保证数据全部发送
+                    writer.flush();
+
+                    //退出系统
+                    if (MyClientSocket.QUIT_KEY_WORD.equals(msg)) {
+                        System.out.println("客户端[" + socket.getPort() + "]断开连接");
+                        break;
+                    }
+                }
+
+                reader.close();
+                writer.close();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
